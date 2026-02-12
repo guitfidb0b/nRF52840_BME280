946f978919437be498928593872342a4ef1e7f84
Robert Chavez <chavez.bobby@gmail.com>
2026-02-12 04:57:37 -0500

    Fixed issues with pressure compensation

--- src/main.c
+++ src/main.c
@@ -90,22 +90,28 @@ void bme_calibrationdata(const struct i2c_dt_spec *spec, struct bme280_data *sen
 
 static int32_t bme280_compensate_press(struct bme280_data *data, int32_t adc_press)
 {
-	int32_t var1, var2, p;
-	var1 = t_fine - 128000;
-	var2 = var1 * var1 * ((int32_t)data->dig_p6);
-	var2 = var2 + ((var1 * (int32_t)data->dig_p5) << 17);
-	var2 = var2 + (((int32_t)data->dig_p4) << 35);
-	var1 = ((var1 * var1 * (int32_t)data->dig_p3) >> 8) + ((var1 * (int32_t)data->dig_p2) << 12);
-	var1 = ((((int32_t)1) << 47) + var1) * ((int32_t)data->dig_p1) >> 33;
-	if(var1 == 0)
-	{
-		return 0;
+	int32_t var1, var2;
+	uint32_t p;
+	var1 = (((int32_t)t_fine)>>1) - (int32_t)64000;
+	var2 = (((var1>>2) * (var1>>2)) >> 11) * ((int32_t)data->dig_p6);
+	var2 = var2 + ((var1*((int32_t)data->dig_p5))<<1);
+	var2 = (var2>>2) + (((int32_t)data->dig_p4)<<16);
+	var1 = (((data->dig_p3 * (((var1>>2) * (var1>>2)) >> 13)) >> 3) + ((((int32_t)data->dig_p2) * 
+		var1)>>1)) >>18;
+	var1 = ((((32768+var1)) * ((int32_t)data->dig_p1))>>15);
+	if(var1 == 0){
+		return 0; //Avoid dividing by 0
 	}
-	p = 1048576 - adc_press;
-	p = (((p<<31) - var2) * 3125) / var1;
-	var1 = (((int32_t)data->dig_p9) * (p >> 13) * (p >> 13)) >> 25;
-	var2 = (((int32_t)data->dig_p8) * p) >> 19;
-	p = ((p + var1 + var2) >> 8) + (((int32_t)data->dig_p7) << 4);
+	p = (((uint32_t)(((int32_t)1048576)-adc_press)-(var2>>12)))*3125;
+	if(p < 0x80000000){
+		p = (p << 1) / ((uint32_t)var1);
+	}
+	else{
+		p = (p / (uint32_t)var1) * 2;
+	}
+	var1 = (((int32_t)data->dig_p9) * ((int32_t)(((p>>3) * (p>>3))>>13)))>>12;
+	var2 = (((int32_t)(p>>2)) * ((int32_t)data->dig_p8))>>13;
+	p = (uint32_t)((int32_t)p + ((var1 + var2 + data->dig_p7) >> 4));
 	return p;
 }
 
@@ -125,7 +131,7 @@ static int32_t bme280_compensate_temp(struct bme280_data *data, int32_t adc_temp
 	//set global t_fine for use in press/hum
 	t_fine = var1 + var2;
 
-	return ((var1 + var2) * 5 + 128) >> 8;
+	return (t_fine * 5 + 128) >> 8;
 }
 
 static int32_t bme280_compensate_hum(struct bme280_data *data, int32_t adc_hum)
@@ -139,7 +145,7 @@ static int32_t bme280_compensate_hum(struct bme280_data *data, int32_t adc_hum)
 	var = (var - (((((var >> 15) * (var >> 15)) >> 7) * ((int32_t)data->dig_h1)) >> 4));
 	var = (var < 0 ? 0 : var);
 	var = (var > 419430400 ? 419430400 : var);
-	return var;
+	return (uint32_t)(var >> 12);
 }
 
 int main(void)
@@ -161,14 +167,7 @@ int main(void)
 	uint8_t regs[] = {ID};
 
 	int ret = i2c_write_read_dt(&dev_i2c, regs, 1, &id, 1);
-	//printk("ret: %d\n", ret);
-	//int ret = i2c_write_dt(&dev_i2c, regs, 1);
-	//if(ret != 0){
-	//	printk("Failed to write register %x \n", regs[0]);
-	//	return -1;
-	//}
-
-	//ret = i2c_read_dt(&dev_i2c, &id, 1);
+	
 	printk("Return value: %d\n", ret);
 	if (ret != 0) {
 		printk("Failed to read register %x \n", regs[0]);
@@ -217,8 +216,8 @@ int main(void)
 		//printk("HumVal1: %d, HumVal2: %d\n", press_val[6], press_val[7]);
 
 		/* STEP 12.1 - Put the data read from registers into actual order (see datasheet) */
-		int32_t adc_press = (press_val[0] << 12) | (press_val[1] << 4) | ((press_val[2] >> 4) & 0x0F);
 		int32_t adc_temp = (press_val[3] << 12) | (press_val[4] << 4) | ((press_val[5] >> 4) & 0x0F);
+		int32_t adc_press = (press_val[0] << 12) | (press_val[1] << 4) | ((press_val[2] >> 4) & 0x0F);		
 		int16_t adc_hum = (press_val[6] << 8) | (press_val[7]);
 
 		//printk("ADCPress: %d\n", adc_press);
@@ -226,18 +225,21 @@ int main(void)
 		//printk("ADCHum: %d\n", adc_hum);
 
 		/* STEP 12.2 - Compensate temperature */
-		int32_t comp_press = bme280_compensate_press(&bmedata, adc_press);
 		int32_t comp_temp = bme280_compensate_temp(&bmedata, adc_temp);
+		int32_t comp_press = bme280_compensate_press(&bmedata, adc_press);
 		int32_t comp_hum = bme280_compensate_hum(&bmedata, adc_hum);
+
+		//printk("Comp Temp: %d\n", comp_temp);
+		//printk("Comp Press: %d\n", comp_press);
+		//printk("Comp Hum: %d\n", comp_hum);
 
 		/* STEP 12.3 - Convert temperature */
 		float temperature = (float)comp_temp / 100.0f;
 		double fTemp = (double)temperature * 1.8 + 32;
-		double pressure = (double)comp_press / 256.0f;
+		double pressure = (double)comp_press;
 		double humidity = (double)comp_hum / 1024.0f;
 
 		// Print reading to console
-		//printk("Temperature in Celsius : %8.2f C\n", (double)temperature);
 		printk("Temperature in Fahrenheit : %.2f F\n", fTemp);
 		printk("Pressure in Pascals: %.2f Pa\n", pressure);
 		printk("Humidity: %.2f percent\n", humidity);
